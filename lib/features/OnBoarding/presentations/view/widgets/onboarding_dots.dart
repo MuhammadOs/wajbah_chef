d70052f5441750bb335a93daf86bb7f6656a1605
Muhammad Osama <mo1318@fayoum.edu.eg>
2024-04-18 00:43:54 +0200

    dashboard view merge

--- lib/features/OnBoarding/presentations/view/widgets/onboarding_dots.dart
+++ lib/features/OnBoarding/presentations/view/widgets/onboarding_dots.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
 import '../../../../../core/constants/constants.dart';
 import '../../../data/onboarding_data.dart';
 
